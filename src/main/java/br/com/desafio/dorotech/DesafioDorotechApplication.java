a3549de8e40a578e5b5d86eab9f3cc4a0d17dbb7
Diogo Emannuel <diogoemannuel.rodrigues1@gmail.com>
2024-05-17 15:40:40 -0300

    first commit

---
+++ src/main/java/br/com/desafio/dorotech/DesafioDorotechApplication.java
@@ -0,0 +1,13 @@
+package br.com.desafio.dorotech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioDorotechApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioDorotechApplication.class, args);
+	}
+
+}
